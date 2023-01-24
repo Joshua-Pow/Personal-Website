58199053abce188b14cbaa4cd7935f5f1fea2b64
racefn <jokesjosh@gmail.com>
2023-01-24 00:21:31 -0500

    Changed to nextjs and tailwind

---
+++ my-website/src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import { Inter } from '@next/font/google'
+import Header from 'components/Header'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Joshua's Website</title>
+      </Head>
+
+      <Header/>
+    </div>
+  )
+}
