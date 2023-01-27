0f2a6ceb2ca825ff1610386c0e43557edbb65a07
racefn <jokesjosh@gmail.com>
2023-01-26 23:23:40 -0500

    Fixed styling for mobile on about page

--- components/About.tsx
+++ components/About.tsx
@@ -9,9 +9,9 @@ function About({}: Props) {
       initial={{ opacity: 0 }}
       whileInView={{ opacity: 1 }}
       transition={{ duration: 1.5 }}
-      className="flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
+      className="pt-20 flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center"
     >
-      <h3 className="mb-20 absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl">
+      <h3 className="uppercase mr-8 tracking-[20px] text-gray-500 text-2xl">
         About
       </h3>
 
@@ -20,7 +20,7 @@ function About({}: Props) {
         transition={{ duration: 1.2 }}
         whileInView={{ x: 0, opacity: 1 }}
         src="../aboutMe.jpg"
-        className="-mb-20 md:mb-0 md:mt-20 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]"
+        className="flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[500px] xl:h-[600px]"
       />
 
       <div className="space-y-10 px:0 md:px-10">
