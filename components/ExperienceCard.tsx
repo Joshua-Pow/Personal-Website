dfadd7b9c132c24fb93a397f856ca8997fc9037a
racefn <jokesjosh@gmail.com>
2023-01-27 03:13:41 -0500

    Added fixed import

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import ProgrammingLanguages from "./programmingLanguages";
+import ProgrammingLanguages from "./ProgrammingLanguages";
 
 type Props = {
   image: string;
