e3ca8cc3bb528ea4b0c324073264d4500c72713b
racefn <jokesjosh@gmail.com>
2023-02-02 03:39:35 -0500

    removed uneeded imports and fixed linter stuff

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,22 +1,10 @@
-import { React, use, useEffect, useRef } from "react";
+import React from "react";
 import { SocialIcon } from "react-social-icons";
-import { circIn, circOut, easeIn, easeOut, motion } from "framer-motion";
+import { easeIn, easeOut, motion } from "framer-motion";
 
 type Props = {};
 
 function Header({}: Props) {
-  const spotify = useRef(null);
-
-  useEffect(() => {
-    console.log("spotify ref: ", spotify);
-    var btn = document.querySelector('[data-testid="play-pause-button"]');
-    btn?.click();
-    console.log(
-      "sptf: ",
-      document.querySelector('[data-testid="play-pause-button"]')
-    );
-  });
-
   return (
     <header className="sticky top-0 p-5 flex flex-items-start justify-between max-w-7xl mx-auto z-20 xl:items-center">
       <motion.div
